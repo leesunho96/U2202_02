7cf2075bcec257226bc94db0437d40d17b439eea
leesunho96 <71704395+leesunho96@users.noreply.github.com>
2022-04-19 22:41:48 +0900

    2022_04_19
    FireStorm

--- Source/U2202_02/U2202_02.cpp
+++ Source/U2202_02/U2202_02.cpp
@@ -3,4 +3,4 @@
 #include "U2202_02.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, U2202_02, "U2202_02" );
+
--- Source/U2202_02/U2202_02.h
+++ Source/U2202_02/U2202_02.h
@@ -2,5 +2,19 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "EngineMinimal.h"
+
+
+
+/*
+	매개변수로 ..., ##VA_ARGS
+	=> 가변인자 매크로.
+
+
+	매개변수가 어떻게 주어질 지 모르는 경우 가변인자 매크로 사용하여 매크로 작성 가능.
+	고정 매개변수가 없이 가변인자만 사용하는 경우 __VA_ARGS__로 사용, 고정 매개변수가 존재 하는 경우 ##__VA_ARGS__ 사용
+
+	-> #define Printf(...) Printf(__VA_ARGS__) // 고정 매개변수가 없는 경우
+	-> #define Printf(Format, ...) Printf(Format, #__VA__ARGS__) // 고정 매개변수가 있는 경우.
+*/
 
